37396016c56a21f22339b43ba6cfaac5059ba8e3
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-19 17:39:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-function loadIssue() {
+function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
   var result = template(issues);
   document.getElementsByTagName("main")[0].innerHTML += result;
